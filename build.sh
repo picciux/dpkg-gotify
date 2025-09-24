17f09d64c93cc1075da89e45735547db7591a35b
Matteo Piscitelli <matteo@matteopiscitelli.it>
2025-09-25 00:21:16 +0200

    Listen on port 8627, since running as non-root prevents to liten on port 80.

--- build.sh
+++ build.sh
@@ -7,9 +7,18 @@ set -e
 # Getting binary
 wget https://github.com/gotify/server/releases/download/v2.7.3/gotify-linux-arm64.zip
 unzip gotify-linux-arm64.zip
+mkdir -p $MYPATH/pkgroot/usr/bin
 mv gotify-linux-arm64 $MYPATH/pkgroot/usr/bin/gotify
 chmod 755 $MYPATH/pkgroot/usr/bin/gotify
-rm -rf LICENSE licenses gotify-linux-arm64.zip
+
+# Gzipping changelog
+CL=pkgroot/usr/share/doc/gotify/changelog.Debian
+cat $CL | gzip -9 > $CL.gz
+rm $CL
 
 dpkg-deb --build $MYPATH/pkgroot $MYPATH/../
 
+# Cleanup
+rm -rf LICENSE licenses gotify-linux-arm64.zip
+rm pkgroot/usr/bin/gotify
+gunzip $CL.gz
